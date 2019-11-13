977f8ccf973bd7cad6515954acb12294b2a01b1a
陈庆 <chenqing873@1caifu.com>
2019-11-13 14:43:14 +0800

    首页添加热点专题模块

--- pages/found/found.js
+++ pages/found/found.js
@@ -1,18 +1,81 @@
 // pages/found/found.js
+// require 引入方法
+var util = require('../../utils/util.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    currentTab:0,
+    winWidth:0,
+    winHeight:0,
+    indicatorDots:false,
+    autoplay:true,
+    interval:5000,
+    duration:1000,
+    movies:['1','2','3','4','5','6','7'],
+    imgUrls: ['https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640']
   },
-
+  switchNav:function(e){
+    var id = e.currentTarget.id;
+    this.setData({
+      currentTab:id
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var page = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        console.log(res);
+        page.setData({
+          //屏幕的宽度
+          winWidth:res.windowWidth
+        });
+        page.setData({
+          //屏幕的高度
+          winHeight:res.windowHeight
+        });
+      },
+    });
+    this.loadMovies();
+  },
+  // 加载电影列表数据
+  loadMovies:function(){
+    var page = this;
+    var key = util.getDataKey();
+    wx.request({
+      url: 'https://api.douban.com/v2/movie/in_theaters?apikey='+key,
+      method:GET,
+      header:{
+        "Content-Type":"json"
+      },
+      success:function(res){
+        console.log(res);
+        var subjects = res.data.subjects;
+        var size = subjects.length;//电影总数量
+        //每行放置3个电影，计算出需要多少行
+        var len = parseInt(size/3); 
+        console.log(len);
+        console.log(subjects);
+        //赋值电影列表
+        page.setData({
+          movies:subjects
+        });
+        //设置电影内容高度
+        page.setData({
+          winHeight:(len+1)*230
+        });
+      },
+      fail:function(res){
+        console.log(res);
+      }
+    })
   },
 
   /**
--- pages/home/home.js
+++ pages/home/home.js
@@ -11,6 +11,7 @@ Page({
       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
     tags:['在售','类固定资产','基础设施类','征信类'],
+    tops: [{ 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20190603\/2738c1d7e4a74a8f8aa9204e4851e8f4.jpg', 'title': '一文读懂资管计划' }, { 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20180906\/8895b93f8f2c43c2863871a269c5e478.jpg', 'title': '经济是如何运行的' }, { 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20180418\/85252a9305394049bd7cac08ff01df32.jpg', 'title': '一图带你轻松看懂金交所' }, { 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20180329\/67947b54691d40a79c503adf243558ac.jpg', 'title': '私募股权投资' }, { 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20180323\/3d40239302344ca2a9308ce2a09c2118.jpg', 'title': '人工智能四大优势' }, { 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20180315\/bf123ef488c34258a41160a51c4476e8.jpg', 'title': '理财师如何为客户配置资产' }, { 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20180306\/754a7d4541344160b49366b727db15de.jpg', 'title': '私募投资避雷指南' }, { 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20180227\/cba4720c0a7a4e2c9ba508319b0be6af.jpg', 'title': '人工智能' }, { 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20180201\/0f2158154bd34cb5a9e0d65d905a209c.jpg', 'title': '刘鹤世界经济论坛致辞' }, { 'imgurl': 'https://rescdn.xiaohu.in/Upload\/News\/20180129\/742ca390743447c89bbd1e66fcef1dbd.jpg', 'title': '非标私募何去何从' }],
     dots:true, //显示指示面板
     color:"gray", //默认指示显示颜色
     active_color:"white", //选中指示显示颜色
@@ -47,22 +48,22 @@ Page({
     object1.img = 'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg';
     object1.title = '集合资管';
     array[3] = object1;
-    // var object1 = new Object();
-    // object1.img = 'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg';
-    // object1.title = '债权基金';
-    // array[4] = object1;
-    // var object1 = new Object();
-    // object1.img = 'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg';
-    // object1.title = '证券基金';
-    // array[5] = object1;
-    // var object1 = new Object();
-    // object1.img = 'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg';
-    // object1.title = '一键报单';
-    // array[6] = object1;
-    // var object1 = new Object();
-    // object1.img = 'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg';
-    // object1.title = '我的客服';
-    // array[7] = object1;
+    var object1 = new Object();
+    object1.img = 'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg';
+    object1.title = '债权基金';
+    array[4] = object1;
+    var object1 = new Object();
+    object1.img = 'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg';
+    object1.title = '证券基金';
+    array[5] = object1;
+    var object1 = new Object();
+    object1.img = 'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg';
+    object1.title = '一键报单';
+    array[6] = object1;
+    var object1 = new Object();
+    object1.img = 'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg';
+    object1.title = '我的客服';
+    array[7] = object1;
     return array
   },
   // 轮播图滚动后会触发 bindChange 方法
